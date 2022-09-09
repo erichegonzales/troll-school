e1f7da3a129748f37ddfd4837a1dedc0a0992266
riley soloner <rileysoloner@gmail.com>
2022-09-09 16:20:21 -0400

    question table and controller generated.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :courses, only: [:index, :show]
   resources :chapters, only: [:index, :show]
+  resources :questions, only: [:index, :show]
+  resources :quizzes, only: [:index, :show]
 end
