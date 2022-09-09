eb7e41e5b26664173c5b94480009b5d3610c4e56
andor <latandor@gmail.com>
2022-09-09 13:00:10 -0400

    Router and Navbar base added

--- client/src/component/App.jsx
+++ client/src/component/App.jsx
@@ -1,9 +1,23 @@
 import './App.css';
+import { Switch, Route } from 'react-router-dom'
+import  Courses from './Courses'
+import  HomePage from './HomePage'
+import  Profile from './Profile'
 
-function App() {
+const App = () => {
   return (
     <div>
-
+      <Switch>
+        <Route path="/courses">
+          <Courses/>
+        </Route>
+        <Route path="/homePage">
+          <HomePage/>
+        </Route>
+        <Route path="/profile">
+          <Profile/>
+        </Route>
+      </Switch>
     </div>
   );
 }
--- client/src/component/Courses.jsx
+++ client/src/component/Courses.jsx
@@ -1,4 +1,4 @@
-function Courses() {
+const Courses = () => {
   return (
     <div>
 
