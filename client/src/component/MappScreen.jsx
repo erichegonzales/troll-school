c7696c0b7fc5791f749a0fa18fa9b5e9e66bb833
andor <latandor@gmail.com>
2022-09-11 22:33:27 -0400

    mapps changes to css

--- client/src/component/MappScreen.jsx
+++ client/src/component/MappScreen.jsx
@@ -2,13 +2,13 @@ const MappScreen = () => {
     return (
         <div className="mapp-screen">
             <div className="mapp-screen-container">
-                <div className="islands island1">
-                    <div className="level"></div>
-                    <div className="level"></div>
-                    <div className="level"></div>
+                <div className="islands island1 bg-image">
+                    <div className="level level1"></div>
+                    <div className="level level2"></div>
+                    <div className="level level3"></div>
                 </div>
-                <div className="islands island2"></div>
-                <div className="islands island3"></div>
+                <div className="islands island2 bg-image2" ></div>
+                <div className="islands island3 bg-image3"></div>
             </div>
         </div>
     )
