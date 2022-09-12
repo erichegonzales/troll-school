346f0ccf06283d0b524de46480bb0bfffd7fc4f0
Eriche Gonzales <erichelouise@gmail.com>
2022-09-12 15:11:32 -0400

    working on frontend login

--- client/src/component/Login.jsx
+++ client/src/component/Login.jsx
@@ -1,15 +1,31 @@
 import {useState} from 'react'
 
 const Login = () => {
-  const [email, setEmail] =useState('');
-  const [password, setPassword] =useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] =u seState('');
+  const [errors, setErrors] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // setIsLoading(true)
+
+    // const getUsers = async () => {
+    //   let res = await fetch('/login', {
+
+    //   })
+    //   let req = await res.json()
+    // }
+  }
 
   return (
     <div id="login-component">
+
       <h1 id='welcome'>Welcome to Troll Tutor!</h1>
       <h2>If you're already signed up to be a Troll tutor, log in here.</h2>
+     
       <div id='login'>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>email</h2>
       <input class='input' type='email'></input><br />
       <h2>password</h2><h5>(ssshhhh!)</h5>
@@ -17,6 +33,7 @@ const Login = () => {
       <button type='submit'>Let's go!</button>
       </form>
       </div>
+
       <div id='signup'>
       <h2>If you haven't signed up yet but want to, ask a parent or guardian for permission and enter your email here!</h2>
       <form>
--- db/seeds.rb
+++ db/seeds.rb
@@ -28,10 +28,4 @@ question_2 = Question.create(id: 2, number: 2, content: "2 + 4 = ?", correct_ans
 question_3 = Question.create(id: 3, number: 3, content: "3 + 5 = ?", correct_answer: "8", incorrect_answer1: "7", incorrect_answer2: "6", quiz: quiz_1)
 question_4 = Question.create(id: 4, number: 4, content: "5 + 1 = ?", correct_answer: "6", incorrect_answer1: "7", incorrect_answer2: "8", quiz: quiz_1)
 
-# incorrects = incorrect_answer1, incorrect_answer2, question
-# incorrect_1 = Incorrect.create(id: 1, incorrect_answer1: "3", incorrect_answer2: "5", question: question_1)
-# incorrect_2 = Incorrect.create(id: 2, incorrect_answer1: "5", incorrect_answer2: "7", question: question_2)
-# incorrect_3 = Incorrect.create(id: 3, incorrect_answer1: "7", incorrect_answer2: "6", question: question_3)
-# incorrect_4 = Incorrect.create(id: 4, incorrect_answer1: "7", incorrect_answer2: "8", question: question_4)
-
 puts 'Done seeding!'
