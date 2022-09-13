0cc0abfc33a05d03be0fef9aad2e7197feb43ce7
andor <latandor@gmail.com>
2022-09-13 16:39:45 -0400

    Home Page changes

--- client/src/component/HomePage.jsx
+++ client/src/component/HomePage.jsx
@@ -5,7 +5,7 @@ const HomePage = () => {
     <div className="home-page">
       <div className='home-page-container'>
         <div className='welcome-image'>
-          <img src="https://o.remove.bg/downloads/eec5d83a-0559-48e6-9490-9b4e253dfc37/imagens-creek-trolls-creek-troll-a-little-positivity-11562911157rnx6onkfxm-removebg-preview.png" alt="troll1" className='troll'/>
+          <img src="troll1.png" alt="troll1" className='troll'/>
         </div>
         <div className='welcome-message'>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam ea repudiandae optio nemo facere sint dolorem vel ipsum exercitationem esse rerum quae voluptates at, in unde ad error dolores nostrum.</p>
