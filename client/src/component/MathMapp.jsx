372d47db06835e34df7d73fceead9c9339b6c2cd
andor <latandor@gmail.com>
2022-09-14 15:47:42 -0400

    routes added to mapps

--- client/src/component/MathMapp.jsx
+++ client/src/component/MathMapp.jsx
@@ -1,27 +1,43 @@
-
 import { Link } from "react-router-dom"
+import { useEffect, useState } from "react"
+
+
 const MathMapp = () => {
 
+    const [questions, setQuestions] = useState([]);
 
 
+//     useEffect(() => {
+//     fetch("http://localhost:3000/questions")
+//       .then((r) => r.json())
+//       .then(setQuestions);
+//   }, []);
 
+
+    // console.log(questions[1].id)
     return (
         <div className="mapp-screen">
             <div className="mapp-screen-container">
                 <div className="islands island1 bg-image">
-                    <Link to="/courses/math-mapp/quizzes" className="level level1"><div >1</div></Link>
-                    <div className="level level2">3</div>
-                    <div className="level level3">2</div>
+                    <Link to="/courses/quizzes/1" className="level level1"><div >1</div></Link>
+                    <Link to="/courses/quizzes/2" className="level level2"><div >2</div></Link>
+                    <Link to="/courses/quizzes/3" className="level level3"><div >3</div></Link>
+                    {/* <div className="level level2">3< /div>
+                    <div className="level level3">2</div> */}
                 </div>
                 <div className="islands island2 bg-image2" >
-                    <div className="level level1">4</div>
+                    <Link to="/courses/quizzes/1" className="level level1"><div >4</div></Link>
+                    <Link to="/courses/quizzes/1" className="level level2"><div >5</div></Link>
+                    <Link to="/courses/quizzes/1" className="level level3"><div >6</div></Link>
+                    {/* <div className="level level1">4</div>
                     <div className="level level2">5</div>
-                    <div className="level level3">6</div>
+                    <div className="level level3">6</div> */}
                 </div>
                 <div className="islands island3 bg-image3">
-                    
-                    <div className="level level2">8</div>
-                    <div className="level level3">7</div>
+                    <Link to="/courses/math-mapp/questions/1" className="level level2"><div >7</div></Link>
+                    <Link to="/courses/math-mapp/questions/1" className="level level3"><div >8</div></Link>
+                    {/* <div className="level level2">8</div>
+                    <div className="level level3">7</div> */}
                 </div>
             </div>
         </div>
--- client/src/component/SpellingMapp.jsx
+++ client/src/component/SpellingMapp.jsx
@@ -9,18 +9,26 @@ const SpellingMapp = () => {
         <div className="mapp-screen">
             <div className="mapp-screen-container">
                 <div className="islands island4 bg-image4">
-                    <div className="level level4">3</div>
-                    <div className="level level3">2</div>
+                    <Link to="/courses/quizzes/3" className="level level1"><div >9</div></Link>
+                    <Link to="/courses/quizzes/4" className="level level2"><div >10</div></Link>
+                    {/* <div className="level level4">3</div>
+                    <div className="level level3">2</div> */}
                 </div>
                 <div className="islands island5 bg-image5" >
-                    <div className="level level2">4</div>
+                    <Link to="/courses/math-mapp/questions/1" className="level level2"><div >11</div></Link>
+                    <Link to="/courses/math-mapp/questions/1" className="level level3"><div >12</div></Link>
+                    <Link to="/courses/math-mapp/questions/1" className="level level5"><div >13</div></Link>
+                    {/* <div className="level level2">4</div>
                     <div className="level level3">5</div>
-                    <div className="level level5">6</div>
+                    <div className="level level5">6</div> */}
                 </div>
                 <div className="islands island6 bg-image6">
-                    <div className="level level2">8</div>
+                    <Link to="/courses/math-mapp/questions/1" className="level level2"><div >14</div></Link>
+                    <Link to="/courses/math-mapp/questions/1" className="level level3"><div >15</div></Link>
+                    <Link to="/courses/math-mapp/questions/1" className="level level6"><div >16</div></Link>
+                    {/* <div className="level level2">8</div>
                     <div className="level level3">7</div>
-                    <div className="level level6">7</div>
+                    <div className="level level6">7</div> */}
                 </div>
             </div>
         </div>
