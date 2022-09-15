f22f65e2ce0deea99c18046dc2898d1ea663a9fe
riley soloner <rileysoloner@gmail.com>
2022-09-15 18:18:50 -0400

    updated CSS style for login/signup page

--- client/src/component/Login.jsx
+++ client/src/component/Login.jsx
@@ -6,24 +6,24 @@ const Login = () => {
 
   return (
     <div id="login-component">
-      <h1 id='welcome'>Welcome to Troll Tutor!</h1>
-      <h2>If you're already signed up to be a Troll tutor, log in here.</h2>
       <div id='login'>
+      <h2 id='welcome'>Login / Signup</h2>
+      <h2>If you have a user account, log in here.</h2>
       <form>
-        <h2>email</h2>
-      <input className='input' type='email'></input><br />
-      <h2>password</h2><h5>(ssshhhh!)</h5>
-      <input className='input' type='password'></input><br />
+        <h2>email:</h2>
+      <input className='input' type='email' placeholder="name@example.com"></input><br />
+      <h2>password:</h2>
+      <input className='input' type='password' placeholder="password"></input><br />
       <button type='submit'>Let's go!</button>
       </form>
       </div>
       <div id='signup'>
-      <h2>If you haven't signed up yet but want to, ask a parent or guardian for permission and enter your email here!</h2>
+      <h2>If you don't have a user account, <br /> ask a parent or guardian to enter a valid email address here!</h2>
       <form>
         <h2>email</h2>
-      <input className='input' type='email'></input><br />
+          <input className='input' type='email' placeholder="name@example.com"></input><br />
       <h2>password</h2>
-      <input className='input' type='password'></input><br />
+          <input className='input' type='password' placeholder="password"></input><br />
       <button type='submit'>Sign me up!</button>
       </form>
 
