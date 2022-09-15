d46761cb1102bbee3c28f31ebb9a333753fa54de
andor <latandor@gmail.com>
2022-09-15 18:55:20 -0400

    Map images added

--- client/src/component/ArtMap.jsx
+++ client/src/component/ArtMap.jsx
@@ -22,14 +22,14 @@ const [description, setDescription] = useState("")
     <div className="mapp-screen">
       <div className="mapp-screen-container">
         <div className="spelling-mapp-desc"><p>{description}</p></div>
-        <div className="islands island4 bg-image4">
+        <div className="islands island4 bg-image7">
           <Link to="/courses/art-mapp/questions/1" className="level level2"><div >1</div></Link>
           <Link to="/courses/art-mapp/questions/1" className="level level3"><div >2</div></Link>
           <Link to="/courses/art-mapp/questions/1" className="level level1"><div >3</div></Link>
           {/* <div className="level level4">3</div>
                     <div className="level level3">2</div> */}
         </div>
-        <div className="islands island5 bg-image5" >
+        <div className="islands island5 bg-image8" >
           <Link to="/courses/art-mapp/questions/1" className="level level2"><div >3</div></Link>
           <Link to="/courses/art-mapp/questions/1" className="level level3"><div >4</div></Link>
           <Link to="/courses/art-mapp/questions/1" className="level level1"><div >5</div></Link>
@@ -37,8 +37,8 @@ const [description, setDescription] = useState("")
                     <div className="level level3">5</div>
                     <div className="level level5">6</div> */}
         </div>
-        <div className="islands island6 bg-image6">
-          <Link to="/courses/art-mapp/questions/1" className="level level2"><div >6</div></Link>
+        <div className="islands island6 bg-image9">
+          <Link to="/courses/art-mapp/questions/1" className="level level1"><div >6</div></Link>
           <Link to="/courses/art-mapp/questions/1" className="level level3"><div >7</div></Link>
 
         </div>
--- client/src/component/HistoryMap.jsx
+++ client/src/component/HistoryMap.jsx
@@ -19,25 +19,23 @@ const [description, setDescription] = useState("")
     <div className="mapp-screen">
       <div className="mapp-screen-container">
         <div className="spelling-mapp-desc"><p>{description}</p></div>
-        <div className="islands island4 bg-image4">
-          <Link to="/courses/history-mapp/questions/1" className="level level2"><div >3</div></Link>
-          <Link to="/courses/history-mapp/questions/1" className="level level3"><div >4</div></Link>
-          <Link to="/courses/history-mapp/questions/1" className="level level1  "><div >5</div></Link>
+        <div className="islands island4 bg-image10">
+          <Link to="/courses/history-mapp/questions/1" className="level level4"><div >1</div></Link>
+          <Link to="/courses/history-mapp/questions/1" className="level level3"><div >2</div></Link>
           {/* <div className="level level4">3</div>
                     <div className="level level3">2</div> */}
         </div>
-        <div className="islands island5 bg-image5" >
+        <div className="islands island5 bg-image11" >
           <Link to="/courses/history-mapp/questions/1" className="level level2"><div >3</div></Link>
           <Link to="/courses/history-mapp/questions/1" className="level level3"><div >4</div></Link>
-          <Link to="/courses/history-mapp/questions/1" className="level level5"><div >5</div></Link>
+          <Link to="/courses/history-mapp/questions/1" className="level level4"><div >5</div></Link>
           {/* <div className="level level2">4</div>
                     <div className="level level3">5</div>
                     <div className="level level5">6</div> */}
         </div>
-        <div className="islands island6 bg-image6">
-          <Link to="/courses/history-mapp/questions/1" className="level level2"><div >6</div></Link>
-          <Link to="/courses/history-mapp/questions/1" className="level level3"><div >7</div></Link>
-          <Link to="/courses/history-mapp/questions/1" className="level level6"><div >8</div></Link>
+        <div className="islands island6 bg-image12">
+          <Link to="/courses/history-mapp/questions/1" className="level level3"><div >6</div></Link>
+          <Link to="/courses/history-mapp/questions/1" className="level level4"><div >7</div></Link>
         </div>
       </div>
     </div>
