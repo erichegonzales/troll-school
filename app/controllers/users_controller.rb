cd5c838f62fb0c0ab3e724200c34003678ae649a
Eriche Gonzales <erichelouise@gmail.com>
2022-09-15 23:07:55 -0400

    adding files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,62 +1,23 @@
 class UsersController < ApplicationController
-    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-    skip_before_action :verify_authenticity_token, only: [:list_create, :list_destroy]
-    # rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    skip_before_action :authorized, only: [:create]
 
-    def index
-        render json: User.all
+    def profile
+        render json: { user: UserSerializer.new(current_user) }, status: :accepted
     end
 
-    def show
-        user = find_user
-        render json: user
-    end
-
-    def list_index
-        user = find_user
-        completeds = user.completeds
-        output = []
-        completeds.each do |comp| 
-            quiz = Quiz.find(comp.quiz.id)
-            output.push({course: quiz.course.name, quiz: quiz.id})
-        end
-        render json: {
-            quizzes: output
-        }
-    end
-
-    def list_create
-        user = find_user
-        quiz = Quiz.find(params[:quid])
-        if (user.completeds.find_by(quiz: quiz)) 
-            render json: user.completeds
-        else 
-            completed = Completed.new(user: user, quiz: quiz)
-            if completed.save
-                render json: user.completeds
-            else
-                render json: {error: "invalid user or quiz id"}, status: 422
-            end
+    def create
+        @user = User.create(user_params)
+        if @user.valid?
+        @token = encode_token({ user_id: @user.id })
+        render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+        else
+        render json: { error: 'failed to create user' }, status: :unprocessable_entity
         end
     end
 
-    def list_destroy
-        user = find_user
-        user.completeds.each do |comp| comp.destroy end
-        render json: user.completeds
-    end
+    private
 
-    # private
-    
-    def find_user
-        user = User.find(params[:id])
+    def user_params
+        params.permit(:name, :username, :email, :password, :password_confirmation, :avatar)
     end
-
-    def render_not_found_response
-        render json: { error: "User not found" }, status: :not_found
-    end
-
-    # def render_unprocessable_entity_response(invalid)
-    #     render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
-    # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show]
   resources :courses, only: [:index, :show]
   resources :user_courses, only: [:index, :show]
   resources :questions, only: [:index, :show]
   resources :quizzes, only: [:index, :show]
 
-  get "/courses/:id/quiz/:quid", to: "courses#quiz"
-
-  get "/users/:id/list", to: "users#list_index"
-  post "/users/:id/list/add/:quid", to: "users#list_create"
-  delete "/users/:id/list", to: "users#list_destroy"
+  resources :users, only: [:create]
+  post '/login', to: 'auth#create'
+  post "/signup", to: "users#create"
+  get '/profile', to: 'users#profile'
 end
